9d38cb679f023d23f5ed904e3a8baa93f947ab53
Manuel <Manuel@Msito>
2020-02-25 19:40:26 +0100

    Cambiando nombre de variables y metodos.

--- src/tareapresencialed_manueltorres/TareaPresencialED_ManuelTorres.java
+++ src/tareapresencialed_manueltorres/TareaPresencialED_ManuelTorres.java
@@ -2,11 +2,11 @@ package tareapresencial2ev;
 
 public class CaminoBasico {
 
-    public int miMetodo(int a, int b) {
+    public int camino(int numero1, int numero2) {
         int resultado = 0;
-        if (a > 5 && b > 10) {
+        if (numero1 > 5 && numero2 > 10) {
             resultado = 55;
-        } else if (b < 10) {
+        } else if (numero2 < 10) {
             resultado = 100;
         }
         return resultado;
